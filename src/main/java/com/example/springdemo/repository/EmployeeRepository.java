2105bf0d13ad8c8c9048cdc8031baf1331e082eb
Nakladalova <xnakla07@vutbr.cz>
2022-10-20 09:48:29 +0200

    New commit

---
+++ src/main/java/com/example/springdemo/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.springdemo.repository;
+
+import com.example.springdemo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
